1869f96fbef40a218d13e389e5952cfbc32006e0
radicaldxb <56843877+radicaldxb@users.noreply.github.com>
2026-01-28 16:55:51 +0400

    Update route.js
    Security level

--- src/app/api/chatbot/route.js
+++ src/app/api/chatbot/route.js
@@ -21,7 +21,7 @@ export async function POST(req) {
     console.log("Received chatInput:", chatInput);
 
     // --- call n8n webhook ---
-   const n8nRes = await fetch("https://n8n.srv943460.hstgr.cloud/webhook/9880443f-5de1-4ba8-a190-aa6f08f32aca/chat", {
+   const n8nRes = await fetch(process.env.N8N_WEBHOOK_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ chatInput, sessionId, metadata }),
